c648c8c86fb1148ae610bbff05e6641618eed9c2
Mukul Chandrakant Mahadik <mmahadik@asu.edu>
2024-06-14 00:35:10 -0700

    Chapter 15, 16 complete: Authentication + Metadata added.

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -4,7 +4,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-import Form from '../ui/invoices/create-form';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
     id: z.string(),
@@ -41,7 +42,10 @@ export type State = {
     message?: string | null,
 };
 
-export async function createInvoice(prevState: State, formData: FormData) {
+export async function createInvoice(
+    prevState: State,
+    formData: FormData,
+) {
     // const rawFormData = {
     // const { customerId, amount, status } = CreateInvoice.parse({
 
@@ -87,7 +91,11 @@ export async function createInvoice(prevState: State, formData: FormData) {
     redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+export async function updateInvoice(
+    id: string,
+    prevState: State,
+    formData: FormData,
+) {
     // const { customerId, amount, status } = UpdateInvoice.parse({
 
     // Validated fields using Zod
@@ -127,9 +135,10 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
 }
 
 
-export async function deleteInvoice(id: string) {
+export async function deleteInvoice(
+    id: string
+) {
     // throw new Error('Failed to Delete Invoice');
-
     try {
         await sql`
             DELETE FROM invoices
@@ -142,4 +151,23 @@ export async function deleteInvoice(id: string) {
     }
 
     revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return "Invalid credentials.";
+                default:
+                    return "Something went wrong.";
+            }
+        }
+        throw error;
+    }
 }
